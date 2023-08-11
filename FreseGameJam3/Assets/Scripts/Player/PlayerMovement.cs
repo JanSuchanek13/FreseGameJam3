29996ca0d6d5feb6c3803ac6547724028eace7ac
JanSuchanek13 <82054660+JanSuchanek13@users.noreply.github.com>
2023-08-11 22:19:42 +0200

    Update Jan
    Player Prefab

--- FreseGameJam3/Assets/Scripts/Player/PlayerMovement.cs
+++ FreseGameJam3/Assets/Scripts/Player/PlayerMovement.cs
@@ -39,11 +39,28 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Movement();
+        FaceMouse();
     }
 
     private void Movement()
     {
         inputMovement = new Vector3(playerInput.Player.Movement.ReadValue<Vector2>().x, 0, playerInput.Player.Movement.ReadValue<Vector2>().y);
         transform.Translate(inputMovement * Time.deltaTime * moveSpeed, Space.World);
+    }
+
+    private void FaceMouse()
+    {
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+            Debug.DrawRay(transform.position, gameObject.transform.forward * 100, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
     }
 }
