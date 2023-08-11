d65a92f66984d1a8535bb9db379bc97e82b7eae5
JanSuchanek13 <82054660+JanSuchanek13@users.noreply.github.com>
2023-08-12 01:00:15 +0200

    Update Jan
    first firemode in PlayerShooting

--- FreseGameJam3/Assets/Scripts/Player/PlayerShooting.cs
+++ FreseGameJam3/Assets/Scripts/Player/PlayerShooting.cs
@@ -8,12 +8,16 @@ public class PlayerShooting : MonoBehaviour
     [Tooltip("Reference to the PlayerInput Action Mapping")]
     private PlayerInput playerInput;
     private WeaponHandler holdingWeapons;
+    private WeaponScriptableObject weapon1Data;
+    private WeaponScriptableObject weapon2Data;
 
+    private float nextShot;
 
     private void Awake()
     {
         playerInput = new PlayerInput();
         holdingWeapons = GetComponent<WeaponHandler>();
+        UpdateWeaponData();
     }
 
     /// <summary>
@@ -34,14 +38,33 @@ public class PlayerShooting : MonoBehaviour
 
     private void Update()
     {
-        if (playerInput.Player.Shoot.triggered)
-        {
-            Debug.Log(holdingWeapons.Weapon1.GetComponent<WeaponData>().data.damage);
-        }
+        UpdateWeaponData();//das später in WeaponData aufrüfen sobald eine Waffe wechselt
+        ShootingManager();
+        
     }
 
-    private void Shoot()
+    public void UpdateWeaponData()
     {
+        weapon1Data = holdingWeapons.Weapon1.GetComponent<WeaponData>().data;
+        weapon2Data = holdingWeapons.Weapon2.GetComponent<WeaponData>().data;
+    }
 
+    private void ShootingManager()
+    {
+        switch (weapon1Data.fireMode)
+        {
+            case 0:
+                if (playerInput.Player.Shoot.triggered)
+                {
+                    if(Time.time > nextShot)
+                    {
+                        nextShot = Time.time + weapon1Data.firingRate;
+                        Debug.Log(holdingWeapons.Weapon1.GetComponent<WeaponData>().data.damage);
+                    }
+                    
+                }
+                break;
+
+        }
     }
 }
